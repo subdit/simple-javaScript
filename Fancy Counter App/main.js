e6ab6d961cbc8be04c27b702ada25cf403478298
Kasidit Bualoeng <subdit@gmail.com>
2023-11-01 10:32:39 -0400

    add funciton on decreaseButton and set reset button to 0

--- Fancy Counter App/main.js
+++ Fancy Counter App/main.js
@@ -1,17 +1,31 @@
-const increaseButtonE1 = document.querySelector('.counter__button--increase');
-const counterValueE1 = document.querySelector('.counter__value');
+const increaseButtonEl = document.querySelector('.counter__button--increase');
+const decreaseButtonEl = document.querySelector('.counter__button--decrease');
+const counterValueEl = document.querySelector('.counter__value');
+const resetButtonEl = document.querySelector('.counter__reset-button');
 
-increaseButtonE1.addEventListener('click', function () {
+resetButtonEl.addEventListener('click', function () {
+  // set counter value to 0
+  counterValueEl.textContent = 0;
+});
+
+decreaseButtonEl.addEventListener('click', function () {
   // Get current value of counter on Click
-  const currentValue = counterValueE1.textContent;
+  const currentValue = counterValueEl.textContent;
   // convert value to number
   const currentValueAsNumber = +currentValue;
+  // decrement by 1
+  const newValue = currentValueAsNumber - 1;
+  // set counter element with new value
+  counterValueEl.textContent = newValue;
+});
 
+increaseButtonEl.addEventListener('click', function () {
+  // Get current value of counter on Click
+  const currentValue = counterValueEl.textContent;
+  // convert value to number
+  const currentValueAsNumber = +currentValue;
   // increment by 1
   const newValue = currentValueAsNumber + 1;
   // set counter element with new value
-  counterValueE1.textContent = newValue;
+  counterValueEl.textContent = newValue;
 });
-
-// const decreaseButtonE2 = document.querySelector('.counter__button--decrease');
-// console.log(decreaseButtonE2);
