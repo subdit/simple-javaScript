4b079516eb7af61a7accec5d330a87758496c5ab
Kasidit Bualoeng <subdit@gmail.com>
2023-11-03 10:53:02 -0400

    add logic for character counting to subtrack from twitter adn facebook character count

--- Word Analytics/script.js
+++ Word Analytics/script.js
@@ -1,11 +1,17 @@
 const textareaEl = document.querySelector('.textarea');
+const characterNumberEl = document.querySelector('.stat__number--characters');
+const twitterNumberEl = document.querySelector('.stat__number--twitter');
+const facebookNumberEl = document.querySelector('.stat__number--facebook');
+const wordNumberEl = document.querySelector('.stat__number--words');
 
 textareaEl.addEventListener('input', function () {
-  // determine the characters and numbers
-  const getNumberOffCharacters = textareaEl.value.length;
-  // set new numbers
-  const countCharactersNumberEl = document.querySelector(
-    '.stat__number--characters'
-  );
-  countCharactersNumberEl.textContent = getNumberOffCharacters;
+  // get new numbers of the character
+  const numbersOfCharacters = textareaEl.value.length;
+  const twitterCharacterLeft = 280 - numbersOfCharacters;
+  const facebookCharacterLeft = 2200 - numbersOfCharacters;
+  // Set counting new numbers
+
+  characterNumberEl.textContent = numbersOfCharacters;
+  twitterNumberEl.textContent = twitterCharacterLeft;
+  facebookNumberEl.textContent = facebookCharacterLeft;
 });
