3a86555d1d55330211c262c4383914d1b2794c59
Kasidit Bualoeng <subdit@gmail.com>
2023-11-03 12:52:43 -0400

    add visual logic on TwitterCharacterLeft and facebookCharacterLeft and add stat__number--limit on styles.css file

--- Word Analytics/script.js
+++ Word Analytics/script.js
@@ -8,7 +8,19 @@ textareaEl.addEventListener('input', function () {
   // get new numbers of the character
   const numbersOfCharacters = textareaEl.value.length;
   const twitterCharacterLeft = 280 - numbersOfCharacters;
-  const facebookCharacterLeft = 2200 - numbersOfCharacters;
+  const facebookCharacterLeft = 200 - numbersOfCharacters;
+  // Add logic if limit character is excessed
+  if (twitterCharacterLeft < 0) {
+    twitterNumberEl.classList.add('stat__number--limit');
+  } else {
+    twitterNumberEl.classList.remove('stat__number--limit'); // to remove  the class other wise it will stay on Red.
+  }
+
+  if (twitterCharacterLeft < 0) {
+    facebookNumberEl.classList.add('stat__number--limit');
+  } else {
+    facebookNumberEl.classList.remove('stat__number--limit'); // to remove  the class other wise it will stay on Red.
+  }
   // Set counting new numbers
 
   characterNumberEl.textContent = numbersOfCharacters;
